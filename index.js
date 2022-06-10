708c4e5f1f4092073cbfded7e7f39d5b30d2923d
Camila Zagal <camilazags@gmail.com>
2022-06-09 21:17:12 -0500

    created questions array

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ const employeeTemp = require("./templates/employee");
 const managerTemp = require("./templates/manager");
 const engineerTemp = require("./templates/engineer");
 
+const questions = [
+    {
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    },{
+        type: "input",
+        message: "What is your ID number?",
+        name: "numberId"
+    },{
+        type: "input",
+        message: "What is your email?",
+        name: "mail"
+    },{
+        type: "list",
+        message: "What is your role within the team?",
+        name: "role",
+        choices: [
+            "Employee",
+            "Manager",
+            "Engineer",
+            "Intern",
+        ],
+    }
+];
