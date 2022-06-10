353f2234d239940336b9484d0b55dae157bfc1d7
Camila Zagal <camilazags@gmail.com>
2022-06-09 20:10:36 -0500

    exported templates

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+//Libraries
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+//Templates
+const internTemp = require("./templates/intern");
+const employeeTemp = require("./templates/employee");
+const managerTemp = require("./templates/manager");
+const engineerTemp = require("./templates/engineer");
+
